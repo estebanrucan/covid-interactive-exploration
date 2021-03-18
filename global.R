0fda0a4ae951ae02483a61eee7117646bcc3b8ff
estebanrucan <errucan@uc.cl>
2021-03-17 21:39:07 -0300

    Efficiency improvements

--- global.R
+++ global.R
@@ -1,36 +1,35 @@
-require(tidyverse)
-require(shiny)
-require(flexdashboard)
-require(magrittr)
-require(lubridate)
-require(purrr)
-require(janitor)
-require(leaflet)
-require(ggrepel)
-require(slider)
-require(plotly)
-require(scales)
-require(rvest)
-require(httr)
-
+suppressPackageStartupMessages(require(tidyverse))
+suppressPackageStartupMessages(require(shiny))
+suppressPackageStartupMessages(require(flexdashboard))
+suppressPackageStartupMessages(require(magrittr))
+suppressPackageStartupMessages(require(lubridate))
+suppressPackageStartupMessages(require(purrr))
+suppressPackageStartupMessages(require(janitor))
+suppressPackageStartupMessages(require(leaflet))
+suppressPackageStartupMessages(require(ggrepel))
+suppressPackageStartupMessages(require(slider))
+suppressPackageStartupMessages(require(plotly))
+suppressPackageStartupMessages(require(scales))
+suppressPackageStartupMessages(require(rvest))
+suppressPackageStartupMessages(require(httr))
 
 
 # load data ---------------------------------------------------------------
 
-page <- GET('https://github.com/owid/covid-19-data/commits/master/public/data/owid-covid-data.csv',
+commits_page <- GET('https://github.com/owid/covid-19-data/commits/master/public/data/owid-covid-data.csv',
             user_agent('Esteban :)'))
 
 
 
-dates <- page %>% 
+dates <- commits_page %>% 
     content() %>% 
     html_nodes('relative-time.no-wrap') %>% 
     html_attr('datetime') %>%
     as_datetime()
 
-last_commit <- dates[1]
+last_commit  <- dates[1]
 
-last_update <- read_csv(file.path('datasets', 'date.csv')) %>% pull(date)
+last_update  <- read_rds(file.path('datasets', 'date.RDS')) %>% pull(date)
 
 current_time <- Sys.time() %>% with_tz('UTC') 
 
@@ -38,19 +37,14 @@ time_since_last_commit <- interval(last_commit, current_time) %/% hours(1)
 
 if (last_update < last_commit) {
     covid_data <- read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
-    write.csv(covid_data, file = file.path('datasets', 'owid-covid-data.csv'))
-    write.csv(last_commit %>% tibble(date = .), file = file.path('datasets', 'date.csv'))
+    saveRDS(covid_data, file = file.path('datasets', 'owid-covid-data.RDS'))
+    saveRDS(last_commit %>% tibble(date = .), file = file.path('datasets', 'date.RDS'))
 } else {
-    covid_data <- read_csv(file.path('datasets', 'owid-covid-data.csv'))
+    covid_data <- read_rds(file.path('datasets', 'owid-covid-data.RDS'))
 }
 
-coordinates <- read_csv(file.path('datasets', 'spatial.csv'))
 
-coordinates %<>% 
-    clean_names() %>% 
-    select(iso_code = alpha_3_code,
-           longitude = longitude_average,
-           latitude = latitude_average)
+coordinates <- read_rds('datasets//coordinates.RDS')
 
 covid_data %<>%
     filter(!(location %in% c('World', unique(continent), 'European Union')))
@@ -71,194 +65,5 @@ covid_data %<>%
     select(everything(), total_active) %>% 
     mutate(total_recovered = total_cases - total_active - total_deaths)
 
-# others ------------------------------------------------------------------
-
-
-continents <- covid_data %>% 
-    pull(continent) %>% 
-    unique() %>% 
-    sort()
-
-countries_per_continent <- covid_data %>% 
-    arrange(continent) %>% 
-    group_by(continent) %>% 
-    nest() %>% 
-    mutate(countries = map(data, ~.x %>% 
-                               pull(location) %>%
-                               unique() %>% 
-                               sort())) %>% 
-    pull(countries) %>% 
-    set_names(continents) 
-
-
-    
-
-
-# each country cases ------------------------------------------------------
-
-
-
-country_cases <- function(start, end, top) {
-    covid_data %>%
-        filter(between(date, as.Date(start), as.Date(end))) %>% 
-        replace_na(list(new_cases = 0)) %>%
-        left_join(coordinates, by = 'iso_code') %>% 
-        group_by(continent, location) %>% 
-        summarise(cases = sum(new_cases),
-                  mean_cases = mean(new_cases)) %>% 
-        arrange(desc(cases)) %>% 
-        head(top) %>% 
-        rename('Total Cases' = cases,
-               'Country' = location,
-               Continent = continent,
-               'Mean Cases' = mean_cases)
-        
-}
-
-
-country_cases_pm <- function(start, end, top) {
-    covid_data %>%
-        filter(between(date, as.Date(start), as.Date(end))) %>% 
-        replace_na(list(new_cases = 0)) %>%
-        left_join(coordinates, by = 'iso_code') %>% 
-        group_by(continent, location) %>% 
-        summarise(cases = sum(new_cases),
-                  cases_per_million = round(cases / max(population) * 1e+6)) %>% 
-        arrange(desc(cases_per_million)) %>% 
-        head(top) %>% 
-        rename('Total Cases' = cases,
-               'Country' = location,
-               Continent = continent,
-               'Total Cases per Million' = cases_per_million)
-    
-}
-
-
-global_total_cases <- function(start, end, top) {
-    tab1 <- covid_data %>%
-        filter(between(date, as.Date(start), as.Date(end))) %>% 
-        filter(!is.na(new_cases)) %>% 
-        group_by(location) %>% 
-        summarise(cases = sum(new_cases)) %>% 
-        arrange(desc(cases)) %>% 
-        head(top) %>% 
-        pull(location)
-    
-    covid_data %>%
-        filter(between(date, as.Date(start), as.Date(end))) %>% 
-        filter(!is.na(new_cases)) %>% 
-        filter(location %in% tab1) %>% 
-        group_by(date) %>% 
-        summarise(cases = sum(new_cases)) %>% 
-        ungroup() %>% 
-        mutate(cum_cases = cumsum(cases)) %>% 
-        arrange(date) %>% 
-        rename('Date' = date,
-               'New Cases' = cases,
-               'Accumulated Cases' = cum_cases)
-}
-
-
-
-# map ---------------------------------------------------------------------
-
-
-global_summary <- function(start, end) {
-    covid_data %>%
-        filter(between(date, as.Date(start), as.Date(end))) %>% 
-        replace_na(list(new_cases = 0, new_deaths = 0, new_tests = 0)) %>%
-        group_by(location) %>% 
-        summarise(total_cases = sum(new_cases),
-                  total_deaths = sum(new_deaths),
-                  positivity = round(sum(new_cases) / sum(new_tests) * 100)) %>% 
-        left_join(covid_data %>% 
-                      select(location, iso_code) %>% 
-                      distinct(), by = 'location')  %>% 
-        select(iso_code, location, total_cases, total_deaths, positivity) %>% 
-        left_join(coordinates, by = "iso_code")
-}
-
-
-country_summary <- function(start, end, country) {
-
-    interval_covid_data <- covid_data %>%
-        filter(between(date, as.Date(start), as.Date(end))) %>% 
-        filter(location == country) %>% 
-        replace_na(list(new_cases = 0, new_deaths = 0, new_tests = 0))
-    
-    new_cases <- interval_covid_data %>% 
-        filter(date == as.Date(end)) %>% 
-        pull(new_cases)
-    
-    active_cases <- interval_covid_data %>% 
-        filter(date == as.Date(end)) %>% 
-        pull(total_active)
-    
-    total_recovered <- interval_covid_data %>% 
-        filter(date == as.Date(end)) %>% 
-        pull(total_recovered) 
-    
-    total_cases <- interval_covid_data %>% 
-        filter(date == as.Date(end)) %>% 
-        pull(total_cases) 
-    
-    last_day_deaths <- interval_covid_data %>% 
-        replace_na(list(new_deaths = 0)) %>% 
-        filter(date == as.Date(end)) %>% 
-        pull(new_deaths) 
-    
-    total_deaths <- interval_covid_data %>% 
-        replace_na(list(new_deaths = 0)) %>% 
-        pull(new_deaths) %>% 
-        sum()
-    
-    
-    total_icu <- interval_covid_data %>% 
-        replace_na(list(icu_patients = 0)) %>% 
-        pull(icu_patients) %>% 
-        sum()
-        
-    total_positivity <- interval_covid_data %>% 
-        replace_na(list(new_cases = 0, new_test = 0)) %>% 
-        mutate(total_pos = round(sum(new_cases) / sum(new_tests) * 100, 1)) %>% 
-        pull(total_pos) %>% 
-        max()
-    
-    positivity_last_day <- interval_covid_data %>% 
-        replace_na(list(new_tests = 0, new_cases = 0)) %>% 
-        filter(date == as.Date(end)) %>% 
-        mutate(positive_rate = round(new_cases / new_tests * 100, 1)) %>% 
-        pull(positive_rate)
-    
-    total_test <- interval_covid_data %>% 
-        replace_na(list(new_test = 0)) %>% 
-        pull(new_tests) %>% 
-        sum()
-    
-    test_last_day <- interval_covid_data %>% 
-        filter(date == as.Date(end)) %>%
-        replace_na(list(new_test = 0)) %>% 
-        pull(new_tests)
-    
-    cases_pm <- total_test <- interval_covid_data %>% 
-        replace_na(list(new_cases = 0)) %>% 
-        summarise(ncases_pm = round(sum(new_cases) / max(population) * 1e+6)) %>% 
-        pull(ncases_pm)
-        
-        
-    list(interval_covid_data, new_cases,active_cases, total_recovered, 
-         total_cases, last_day_deaths,
-         total_deaths, total_icu, total_positivity, positivity_last_day, 
-         total_test, test_last_day, cases_pm) %>% 
-        set_names(c(
-            'interval_covid_data', 'new_cases', 'active_cases', 'total_recovered', 
-            'total_cases', 'last_day_deaths', 'total_deaths',
-            'total_icu', 'total_positivity', 'positivity_last_day',
-            'total_test', 'test_last_day', 'cases_pm'
-        ))
-    
-}
-
-
-
-
+covid_data %<>% 
+    left_join(coordinates, by = 'iso_code')
---
+++ statics.R
@@ -0,0 +1,176 @@
+continents <- covid_data %>% 
+    pull(continent) %>% 
+    unique() %>% 
+    sort()
+
+countries_per_continent <- covid_data %>% 
+    arrange(continent) %>% 
+    group_by(continent) %>% 
+    nest() %>% 
+    mutate(countries = map(data, ~.x %>% 
+                               pull(location) %>%
+                               unique() %>% 
+                               sort())) %>% 
+    pull(countries) %>% 
+    set_names(continents) 
+
+# each country cases ------------------------------------------------------
+
+country_cases <- function(start, end, top) {
+    covid_data %>%
+        filter(between(date, as.Date(start), as.Date(end))) %>% 
+        replace_na(list(new_cases = 0)) %>%
+        left_join(coordinates, by = 'iso_code') %>% 
+        group_by(continent, location) %>% 
+        summarise(cases = sum(new_cases),
+                  mean_cases = mean(new_cases)) %>% 
+        arrange(desc(cases)) %>% 
+        head(top) %>% 
+        rename('Total Cases' = cases,
+               'Country' = location,
+               Continent = continent,
+               'Mean Cases' = mean_cases)
+    
+}
+
+
+country_cases_pm <- function(start, end, top) {
+    covid_data %>%
+        filter(between(date, as.Date(start), as.Date(end))) %>% 
+        replace_na(list(new_cases = 0)) %>%
+        group_by(continent, location) %>% 
+        summarise(cases = sum(new_cases),
+                  cases_per_million = round(cases / max(population) * 1e+6)) %>% 
+        arrange(desc(cases_per_million)) %>% 
+        head(top) %>% 
+        rename('Total Cases' = cases,
+               'Country' = location,
+               Continent = continent,
+               'Total Cases per Million' = cases_per_million)
+    
+}
+
+
+global_total_cases <- function(start, end) {
+    covid_data %>%
+        filter(between(date, as.Date(start), as.Date(end))) %>% 
+        filter(!is.na(new_cases)) %>% 
+        group_by(date) %>% 
+        summarise(cases = sum(new_cases)) %>% 
+        ungroup() %>% 
+        mutate(cum_cases = cumsum(cases)) %>% 
+        arrange(date) %>% 
+        rename('Date' = date,
+               'New Cases' = cases,
+               'Accumulated Cases' = cum_cases)
+}
+
+
+
+# map ---------------------------------------------------------------------
+
+
+global_summary <- function(start, end) {
+    covid_data %>%
+        filter(between(date, as.Date(start), as.Date(end))) %>% 
+        replace_na(list(new_cases = 0, new_deaths = 0, new_tests = 0)) %>%
+        group_by(location) %>% 
+        summarise(total_cases = sum(new_cases),
+                  total_deaths = sum(new_deaths),
+                  positivity = round(sum(new_cases) / sum(new_tests) * 100)) %>% 
+        left_join(covid_data %>% 
+                      select(location, iso_code) %>% 
+                      distinct(), by = 'location')  %>% 
+        select(iso_code, location, total_cases, total_deaths, positivity, latitide,
+               longitude, min_lat, min_lon, max_lat, max_lon)
+}
+
+
+country_summary <- function(start, end, country) {
+    
+    interval_covid_data <- covid_data %>%
+        filter(between(date, as.Date(start), as.Date(end))) %>% 
+        filter(location == country) %>% 
+        replace_na(list(new_cases = 0, new_deaths = 0, new_tests = 0))
+    
+    new_cases <- interval_covid_data %>% 
+        filter(date == as.Date(end)) %>% 
+        pull(new_cases)
+    
+    active_cases <- interval_covid_data %>% 
+        filter(date == as.Date(end)) %>% 
+        pull(total_active)
+    
+    total_recovered <- interval_covid_data %>% 
+        filter(date == as.Date(end)) %>% 
+        pull(total_recovered) 
+    
+    total_cases <- interval_covid_data %>% 
+        filter(date == as.Date(end)) %>% 
+        pull(total_cases) 
+    
+    last_day_deaths <- interval_covid_data %>% 
+        replace_na(list(new_deaths = 0)) %>% 
+        filter(date == as.Date(end)) %>% 
+        pull(new_deaths) 
+    
+    total_deaths <- interval_covid_data %>% 
+        replace_na(list(new_deaths = 0)) %>% 
+        pull(new_deaths) %>% 
+        sum()
+    
+    
+    total_icu <- interval_covid_data %>% 
+        replace_na(list(icu_patients = 0)) %>% 
+        pull(icu_patients) %>% 
+        sum()
+    
+    total_positivity <- interval_covid_data %>% 
+        replace_na(list(new_cases = 0, new_test = 0)) %>% 
+        mutate(total_pos = round(sum(new_cases) / sum(new_tests) * 100, 1)) %>% 
+        pull(total_pos) %>% 
+        max()
+    
+    positivity_last_day <- interval_covid_data %>% 
+        replace_na(list(new_tests = 0, new_cases = 0)) %>% 
+        filter(date == as.Date(end)) %>% 
+        mutate(positive_rate = round(new_cases / new_tests * 100, 1)) %>% 
+        pull(positive_rate)
+    
+    total_test <- interval_covid_data %>% 
+        replace_na(list(new_test = 0)) %>% 
+        pull(new_tests) %>% 
+        sum()
+    
+    test_last_day <- interval_covid_data %>% 
+        filter(date == as.Date(end)) %>%
+        replace_na(list(new_test = 0)) %>% 
+        pull(new_tests)
+    
+    cases_pm <- total_test <- interval_covid_data %>% 
+        replace_na(list(new_cases = 0)) %>% 
+        summarise(ncases_pm = round(sum(new_cases) / max(population) * 1e+6)) %>% 
+        pull(ncases_pm)
+    
+    
+    list(interval_covid_data, new_cases,active_cases, total_recovered, 
+         total_cases, last_day_deaths,
+         total_deaths, total_icu, total_positivity, positivity_last_day, 
+         total_test, test_last_day, cases_pm) %>% 
+        set_names(c(
+            'interval_covid_data', 'new_cases', 'active_cases', 'total_recovered', 
+            'total_cases', 'last_day_deaths', 'total_deaths',
+            'total_icu', 'total_positivity', 'positivity_last_day',
+            'total_test', 'test_last_day', 'cases_pm'
+        ))
+    
+}
+
+country_coord <- function(country) {
+    covid_data %>% 
+        filter(location == country) %>% 
+        select(iso_code, location) %>% 
+        slice(1) %>% 
+        left_join(coordinates, by = 'iso_code') %>% 
+        select(longitude, latitude)
+}
